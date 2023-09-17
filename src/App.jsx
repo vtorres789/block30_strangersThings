1744cb9f52101a94435305f6de98feac643c0acd
vtorres789 <victortorres180@gmail.com>
2023-09-16 22:57:46 -0400

    react routes setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import './index.css';
+import { Routes, Route, Link } from 'react-router-dom';
+import Login from './components/Login';
+import Posts from './components/Posts';
+import Profile from './components/Profile';
+import Register from './components/Register';
+import Home from './components/Home';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div id="container">
+        <div id="navbar">
+          <Link to="/">Home</Link>
+          <Link to="/Login" >Login</Link>
+          <Link to="/Posts">Posts</Link>
+          <Link to="/Profile">Profile</Link>
+          <Link to="/Register">Register</Link>
+        </div>
+        <div id="main-section">
+          <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/posts" element={<Posts />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+      <div className="home">
+        <h1>Strangers Things</h1>
+      </div>
+    );
+  }
